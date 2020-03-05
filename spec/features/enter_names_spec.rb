b1b22e6dbe2e42cee2befca2afbe5a1d3ddf8ac9
Peter Stevens <Peterstevens510@gmail.com>
2020-03-05 15:15:28 +0000

    added changes

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -8,6 +8,17 @@ feature 'Enter names' do
   end
 end
 
+feature "hit points" do 
+  scenario 'Player 2s hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Peter'
+    fill_in :player_2_name, with: 'Shrek'
+    click_button 'Submit'
+    expect(page).to have_content '100'
+  end
+end
+
+
 # As two Players,
 # So we can play a personalised game of Battle,
 # We want to Start a fight by entering our names and seeing them
