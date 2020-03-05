8b26d7d9f3fb99771f71716b65270eb789ee0d76
Peter Stevens <Peterstevens510@gmail.com>
2020-03-05 15:41:54 +0000

    added changes

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,19 +1,13 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Peter'
-    fill_in :player_2_name, with: 'Shrek'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Peter vs. Shrek'
   end
 end
 
 feature "hit points" do 
   scenario 'Player 2s hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Peter'
-    fill_in :player_2_name, with: 'Shrek'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content '100'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -103,10 +103,12 @@ ENV['RACK_ENV'] = 'test'
 
 # require our Sinatra app file
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), './features', 'web_helpers.rb')
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
 
 # tell Capybara about our app class
 Capybara.app = Battle
